8bb8c4758585eda99a9ca30657a4b2b2c15cc2b1
Pavel Tsekhanovich <p.tsekhanovich93@gmail.com>
2018-12-05 22:59:03 +0300

    Minor refactoring

--- lesson7/src/main/java/ru/javabegin/training/spring/impls/sony/SonyHead.java
+++ lesson7/src/main/java/ru/javabegin/training/spring/impls/sony/SonyHead.java
@@ -2,10 +2,10 @@ package ru.javabegin.training.spring.impls.sony;
 
 import ru.javabegin.training.spring.interfaces.Head;
 
-public class SonyHead implements Head{
-	
-	public void calc(){
-		System.out.println("Thinking about Sony...");
-	}
+public class SonyHead implements Head {
+
+    public void calc() {
+        System.out.println("Thinking about Sony...");
+    }
 
 }
--- lesson7/src/main/java/ru/javabegin/training/spring/impls/toshiba/ToshibaHand.java
+++ lesson7/src/main/java/ru/javabegin/training/spring/impls/toshiba/ToshibaHand.java
@@ -2,10 +2,10 @@ package ru.javabegin.training.spring.impls.toshiba;
 
 import ru.javabegin.training.spring.interfaces.Hand;
 
-public class ToshibaHand implements Hand{
-	
-	public void catchSomething(){
-		System.out.println("Catched from Toshiba!");
-	}
+public class ToshibaHand implements Hand {
+
+    public void catchSomething() {
+        System.out.println("Catched from Toshiba!");
+    }
 
 }
